69fd68b3186290d4c22f1395df6cd33c906e3cb5
Kevin Judd <125313905+LucidRavings@users.noreply.github.com>
2023-02-22 16:25:11 -0700

    Add files via upload

--- setOne.js
+++ setOne.js
@@ -15,12 +15,22 @@ The code above is an example of 'function declaration.' Please re-write the func
 */
 
 // RE-WRITE THE ABOVE FUNCTION IN 'FUNCTION EXPRESSION' SYNTAX HERE.
-
-
+const findGrape2 = function (arr) {
+    for (i = 0; i < arr.length; i++) {
+        if(arr[i].color === "purple") {
+            return console.log(`The fruit with index ${arr.indexOf(arr[1])} is a grape`)
+        }
+    }
+}
 
 // RE-WRITE THE ABOVE FUNCTION IN 'ARROW FUNCTION' SYNTAX HERE.
-
-
+ findGrape3 = (arr) => {
+    for (i = 0; i < arr.length; i++) {
+        if(arr[i].color === "purple") {
+            return console.log(`The fruit with index ${arr.indexOf(arr[1])} is a grape`)
+        }
+    }
+}
 
 ////////// PROBLEM 2 //////////
 /*
@@ -28,10 +38,10 @@ Write a one line function (give a name of your choice) with an implicit return t
 */
 
 // CODE HERE
-
+whatAreMyParameters = (para1, para2) => console.log(`The first parameter is ${para1}. The second parameter is ${para2}`)
 
 // INVOKE THE FUNCTION HERE. THE PARAMETERS TAKE ANY DATATYPE.
-
+whatAreMyParameters(5, 6)
 
 ////////// PROBLEM 3 //////////
 /*
@@ -40,10 +50,12 @@ Then, outside of the greeting function, invoke the greeting function, passing in
 */
 
 // CODE 'GREETING FUNCTION' HERE
-
+function greeting (firstName, lastName, callback) {
+    callback(firstName + " " + lastName)
+}
 
 // INVOKE 'GREETING FUNCTION' HERE
-
+greeting(`Kevin`, `Judd`, fullName => console.log("Hello, my full name is " + fullName))
 
 ////////// PROBLEM 4 //////////
 
@@ -58,14 +70,20 @@ Write a function called 'pricesPlusTax' that takes 2 params: an array ('prices' 
 */
 
 // CODE HERE
-
+function pricesPlusTax (arr, callback){
+    for(i = 0; i < arr.length; i++){
+        newCost = arr[i] * 1.2
+        totalCost.push(newCost)
+    }
+    callback(totalCost)
+}
 
 /* 
 Invoke the 'pricesPlusTax' function, passing in the 'prices' array and a callback function (passing in 'totalCost' as a param) that will print "The new array plus tax = [totalCost]"
 */
 
 // CODE HERE
-
+pricesPlusTax(prices, totalCost => console.log("The new array pluse tax = " + totalCost))
 
 ////////// PROBLEM 5 //////////
 
@@ -74,18 +92,26 @@ A function can return another function. Let's create one!
 
 Create a function called 'multiplyingFactory' that takes a number as a param. The function returns another function that takes another number as a param. 
 
-The inner function should run this logic: if the first number passing in is greater than and equal to 5, print the multiplication of the first and second numbers. If not, print "Cannot multiply: the first number is smaller than 5." 
+The inner function should run this logic: if the first number passing in is greater than or equal to 5, print the multiplication of the first and second numbers. If not, print "Cannot multiply: the first number is smaller than 5." 
 */
 
 // CODE HERE
-
+function multiplyingFactory (num1){
+    return function funct2 (num2){
+        if ( num1 >= 5){
+            console.log(num1 * num2)
+        }else{
+            console.log(`Cannot multiply: the first number is smaller than 5.`)
+        }
+    }
+}
 
 /* 
 Let's invoke the 'multiplyingFactory' function that will return another function, and save it into a variable called 'timesFour.' Let's pass in number 3 as a param.
 */
 
 // CODE HERE
-
+let timesFour = multiplyingFactory(5)
 
 /* 
 Now, timesFour is the new function (the inner function that was being returned when we invoked 'multiplyingFactory' function). The number 3 that we passed in as a first number is now saved in the 'timesFour' function. 
@@ -96,7 +122,7 @@ Run the code in node to see the printed result. You should see "Cannot multiply:
 */
 
 // INVOKE 'timesFour' HERE
-
+timesFour(4)
 
 /* 
 Change the param for 'multiplyingFactory' invocation to number 5. Then invoke 'timesFour' again, passing in number 4. Run the code in node, and you should see 20.
